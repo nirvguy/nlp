2266dc0cd2e35c851c37c315cd9604efb3507398
Juan Cruz Sosa <nirvguy@gmail.com>
2018-05-20 03:22:50 -0300

    Surnames: Add trainers

---
+++ lstm/classification/trainers.py
@@ -0,0 +1,22 @@
+import torchtrainer
+
+class SupervisedValidator(torchtrainer.SupervisedValidator):
+    def validate_batch(self, lines, lengths, category):
+        output = self.model(lines, lengths)
+
+        for meter in self.meters.values():
+            meter.measure(output.data, category.data)
+
+class SupervisedTrainer(torchtrainer.SupervisedTrainer):
+    def create_validator(self):
+        return SupervisedValidator(self.model, self.val_meters, self.criterion)
+
+    def update_batch(self, lines, lengths, category):
+        self.optimizer.zero_grad()
+        output = self.model(lines, lengths)
+        loss = self.criterion(output, category)
+        loss.backward()
+        self.optimizer.step()
+
+        for meter in self.train_meters.values():
+            meter.measure(output.data, category.data)
