0b736f9e1a13e2bf8612f82d0a20f0a193e9211f
Juan Cruz Sosa <nirvguy@gmail.com>
2018-05-20 03:04:06 -0300

    Surnames: Add LSTM classifier

---
+++ lstm/classification/models.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+from torch.autograd import Variable
+
+class LSTMClassifier(nn.Module):
+    def __init__(self, embedd_dim, hidden_size, vocab_size, classes):
+        super(LSTMClassifier, self).__init__()
+        self.embedding = nn.Embedding(vocab_size, embedd_dim)
+        self.encoder = nn.LSTM(embedd_dim, hidden_size)
+        self.linear = nn.Linear(hidden_size, classes)
+
+    def forward(self, x, lens):
+        x = self.embedding(x)
+        x = pack_padded_sequence(x, lens, batch_first=True)
+        outputs, hidden = self.encoder(x)
+        outputs, _ = pad_packed_sequence(outputs)
+        outputs = torch.stack([outputs[idx-1, i, :] for i, idx in zip(range(outputs.shape[1]), lens)])
+        return self.linear(outputs)
---
+++ surnames/models.py
@@ -0,0 +1,1 @@
+../lstm/classification/models.py
